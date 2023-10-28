1105b57331493d5332c74d9bc250b46b1d6e5527
JackyJerkyYt <jackywongcheukki@gmail.com>
2023-10-28 14:57:08 -0700

    live feed

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,12 @@
+import { api } from '../../convex/_generated/api'
+import { useQuery } from 'convex/react'
 import React from 'react'
 
 export default function Gallery() {
+  const images = useQuery(api.functions.list, )
+  
   return (
-    <div>G</div>
+    <div>{JSON.stringify(images)}</div>
+
   )
 }
